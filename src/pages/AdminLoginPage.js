f0a8162fdfd8dea17e3d586e9a1dce7ce5eee8b7
Harsh Khetia <133527294+harsh01369@users.noreply.github.com>
2025-05-30 21:27:29 +0100

    Update AdminLoginPage.js

--- src/pages/AdminLoginPage.js
+++ src/pages/AdminLoginPage.js
@@ -29,16 +29,11 @@ const AdminLoginPage = ({ setAdminAuthenticated }) => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-<<<<<<< HEAD
             const response = await axios.post(
                 'https://ecommerce-backend.onrender.com/api/admin/login',
                 { username, password },
                 { withCredentials: true }
             );
-=======
-            const response = await axios.post('https://ecommerce-backend.onrender.com/api/admin/login', { username, password }, { withCredentials: true });
->>>>>>> 39e2e44d81558b307a92c2a8bb6e6ff3fedeed23
-
             if (response.status === 200 && response.data.isAdmin) {
                 setAdminAuthenticated(true);
                 navigate('/admin');
@@ -53,7 +48,7 @@ const AdminLoginPage = ({ setAdminAuthenticated }) => {
 
     return (
         <div className="admin-login-page">
-            <h1>UWEAR Admin Login</h1>
+            <h1>Admin Login</h1>
             <form onSubmit={handleLogin}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
